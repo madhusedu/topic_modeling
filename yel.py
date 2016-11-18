528b944ab408d976698f8708f26d289da041a892
madhusedu <madhus911@gmail.com>
2016-11-17 18:16:08 -0800

    Update yel.py

--- yel.py
+++ yel.py
@@ -18,16 +18,6 @@ en_stop = get_stop_words('en')
 # Create p_stemmer of class PorterStemmer
 p_stemmer = PorterStemmer()
 
-# create sample documents
-#doc_a = "Brocolli is good to eat. My brother likes to eat good brocolli, but not my mother."
-#doc_b = "My mother spends a lot of time driving my brother around to baseball practice."
-#doc_c = "Some health experts suggest that driving may cause increased tension and blood pressure."
-#doc_d = "I often feel pressure to perform well at school, but my mother never seems to drive my brother to do better."
-#doc_e = "Health professionals say that brocolli is good for your health."
-
-# compile sample documents into a list
-#doc_set = [doc_a, doc_b, doc_c, doc_d, doc_e]
-
 # list for tokenized documents in loop
 texts = []
 
