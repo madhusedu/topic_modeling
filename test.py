31d6139b95ad493f76eb870edbcef791c5bb7522
bnagesh <bharathnagesh@outlook.com>
2016-11-23 19:58:15 -0800

    andrews code

---
+++ test.py
@@ -0,0 +1,23 @@
+rawdocs = ('eat turkey on turkey day holiday',
+            'i like to eat cake on holiday',
+            'turkey trot race on thanksgiving holiday',
+            'snail race the turtle',
+            'time travel space race',
+            'movie on thanksgiving',
+            'movie at air and space museum is cool movie',
+            'aspiring movie star')
+
+docs = []
+for i in range(0, len(rawdocs)):
+    attach = rawdocs[i].split()
+    for j in range(0, len(attach)):
+        docs.append(attach[j])
+
+
+
+K = 2
+alpha = 1
+eta = .001
+iterations = 3
+
+print docs
